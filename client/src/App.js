3699fab812185df5dcef730d4c51c3efe0934563
Shagens <Hagensshik@gmail.com>
2022-01-09 23:57:58 -0500

    updates and added files

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,9 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 import Home from "./pages/Home";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Nav from "./components/Nav";
-import OrderHistory from "./pages/OrderHistory";
-import Success from "./pages/Success";
+
 
 // importing redux provider and store 
 import { Provider } from 'react-redux';
